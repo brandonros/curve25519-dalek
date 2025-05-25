dbc56e18f6f68383295678d261c0e846722a6fe8
Brandon Ros <brandonros1@gmail.com>
2025-05-25 18:08:46 -0400

    wrong backend

--- curve25519-dalek/src/backend/serial/u64/constants.rs
+++ curve25519-dalek/src/backend/serial/u64/constants.rs
@@ -326,14 +326,19 @@ pub const EIGHT_TORSION_INNER_DOC_HIDDEN: [EdwardsPoint; 8] = [
     },
 ];
 
-/// Table containing precomputed multiples of the Ed25519 basepoint \\(B = (x, 4/5)\\).
-#[cfg(feature = "precomputed-tables")]
-pub static ED25519_BASEPOINT_TABLE: &EdwardsBasepointTable =
+#[cfg(all(feature = "precomputed-tables", feature = "cuda"))]
+#[cuda_std::address_space(global)]
+pub static ED25519_BASEPOINT_TABLE: &'static EdwardsBasepointTable =
+    &ED25519_BASEPOINT_TABLE_INNER_DOC_HIDDEN;
+
+#[cfg(all(feature = "precomputed-tables", not(feature = "cuda")))]
+pub static ED25519_BASEPOINT_TABLE: &'static EdwardsBasepointTable =
     &ED25519_BASEPOINT_TABLE_INNER_DOC_HIDDEN;
 
 /// Inner constant, used to avoid filling the docs with precomputed points.
 #[doc(hidden)]
-#[cfg(feature = "precomputed-tables")]
+#[cfg(feature = "cuda")]
+#[cuda_std::address_space(global)]
 static ED25519_BASEPOINT_TABLE_INNER_DOC_HIDDEN: EdwardsBasepointTable = EdwardsBasepointTable([
     LookupTable([
         AffineNielsPoint {
